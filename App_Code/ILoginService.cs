19a8382dfa15a4c3487e6e7faf0f8b698c45b689
amandaereth <a.ereth5@gmail.com>
2016-02-11 15:49:06 -0800

    commit

---
+++ App_Code/ILoginService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+[ServiceContract]
+public interface ILoginService
+    {
+    [OperationContract]
+    int VenueLogin (string username, string password);
+
+    [OperationContract]
+    int VenueRegistration(VenueLite v);
+    }
+
+	[DataContract]
+public class VenueLite
+    {
+        [DataMember]
+        public string Name {set; get;}
+        [DataMember]
+        public string Address{ set; get; }
+        [DataMember]
+        public string City { set; get; }
+        [DataMember]
+        public string State {set; get;}
+        [DataMember]
+        public string ZipCode {set; get;}
+        [DataMember]
+         public string Phone {set; get;}
+        [DataMember]
+         public string Email {set; get;}
+        [DataMember]
+        public string WebPage { set; get; }
+        [DataMember]
+        public int AgeRestriction { set; get; }
+        [DataMember]
+        public string UserName { set; get; }
+        [DataMember]
+        public string Password { set; get; }
+
+    }
+
